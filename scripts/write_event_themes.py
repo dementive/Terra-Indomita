4086f4aa4fa84d316077f4d67469676525f7614a
dementive <nathanvandrei@yahoo.com>
2023-01-27 18:12:09 -0500

    Nubian background

--- scripts/write_event_themes.py
+++ scripts/write_event_themes.py
@@ -12,7 +12,7 @@ def write_themes_in_dir(path, outfile, outfile2):
 
 	with open(outfile, "w") as file:
 		for i in pic_li:
-			file.write(f"{i}_event_theme = {{\n\ticon = \"gfx/interface/icons/military_traditions/{i}.dds\"\n\tsoundeffect = \"event:/SFX/Events/Generic/sfx_event_generic_uncategorized\"\n}}\n")
+			file.write(f"{i}_event_theme = {{\n\ticon = \"gfx/interface/event_window/Egypt/{i}.dds\"\n\tsoundeffect = \"event:/SFX/Events/Generic/sfx_event_generic_uncategorized\"\n}}\n")
 
 	with open(outfile2, "w") as file:
 		for i in pic_li:
@@ -20,7 +20,10 @@ def write_themes_in_dir(path, outfile, outfile2):
 
 if __name__ == '__main__':
 	# Vanilla
-	write_themes_in_dir("C:\\Program Files (x86)\\Steam\\steamapps\\common\\ImperatorRome\\game\\gfx\\interface\\icons\\military_traditions", "vanilla_out/00_mil_theme_pictures.txt", "vanilla_out/00_mil_pictures.txt")
+	#write_themes_in_dir("C:\\Program Files (x86)\\Steam\\steamapps\\common\\ImperatorRome\\game\\gfx\\interface\\icons\\military_traditions", "vanilla_out/00_mil_theme_pictures.txt", "vanilla_out/00_mil_pictures.txt")
 
 	# Indomita
-	write_themes_in_dir("C:\\Users\\demen\\Documents\\Paradox Interactive\\Imperator\\mod\\Terra-Indomita\\gfx\\interface\\icons\\military_traditions", "mod_out/00_mod_mil_theme_pictures.txt", "mod_out/00_mod_mil_pictures.txt")
+	#write_themes_in_dir("C:\\Users\\demen\\Documents\\Paradox Interactive\\Imperator\\mod\\Terra-Indomita\\gfx\\interface\\icons\\military_traditions", "mod_out/00_mod_mil_theme_pictures.txt", "mod_out/00_mod_mil_pictures.txt")
+
+	# Egypt
+	write_themes_in_dir("C:\\Users\\demen\\Documents\\Paradox Interactive\\Imperator\\mod\\Terra-Indomita\\gfx\\interface\\event_window\\Egypt", "mod_out/00_egypt_theme_pictures.txt", "mod_out/00_egypt_pictures.txt")
