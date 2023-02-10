b755ba27a259c6f25272e0a57de7f94d9f5c902e
dementive <nathanvandrei@yahoo.com>
2023-02-09 20:35:28 -0500

    Added trigger tooltips

--- scripts/make_tooltips.py
+++ scripts/make_tooltips.py
@@ -5,6 +5,6 @@ for i in range(4):
 	for x in range(5):
 		x += 1
 		if x <= 1: 
-			print(f"add_{x}_{pops[i - 1]}_tt:0 \"Add {x} {pops[i - 1]} to [THIS.GetProvince.GetName|Y]\"")
+			print(f"country_has_{x}_{pops[i - 1]}_tt:0 \"[THIS.GetCountry.GetName|Y] has at least #Y {x}#! {pops[i - 1]}\"")
 		else:
-			print(f"add_{x}_{pops[i - 1]}_tt:0 \"Add {x} {pops[i - 1]}s to [THIS.GetProvince.GetName|Y]\"")
+			print(f"country_has_{x}_{pops[i - 1]}_tt:0 \"[THIS.GetCountry.GetName|Y] has at least #Y {x}#! {pops[i - 1]}s\"")
